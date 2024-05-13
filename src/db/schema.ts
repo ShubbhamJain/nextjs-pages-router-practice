e1550092cce64bd7078c6b43b8a960a93323004d
ShubbhamJain <shubham.jain368@gmail.com>
2024-05-14 02:08:59 +0530

    update: schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,24 @@
-import { int, mysqlTable, varchar, text } from "drizzle-orm/mysql-core";
+import {
+  int,
+  mysqlTable,
+  varchar,
+  text,
+  boolean,
+} from "drizzle-orm/mysql-core";
 
 export const users = mysqlTable("users", {
   id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 256 }).notNull(),
+  userName: varchar("userName", { length: 256 }).notNull(),
   email: varchar("email", { length: 256 }).notNull().unique(),
-  password: varchar("email", { length: 256 }).notNull(),
-  profilePic: text("profilePic"),
+  password: varchar("password", { length: 256 }).notNull(),
+  loggedIn: boolean("loggedIn").default(false),
 });
 
 export const comments = mysqlTable("userComments", {
   id: int("id").primaryKey().autoincrement(),
   content: text("content").notNull(),
   likes: int("likes").default(0),
-  // userId: int('userId').references(() => users.id).notNull(),
+  userId: int("userId")
+    .references(() => users.id)
+    .notNull(),
 });
