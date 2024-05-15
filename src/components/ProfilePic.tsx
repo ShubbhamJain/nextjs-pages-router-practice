e278d47f78d46b6fdcf48a8b4aedb2442adbdc1d
ShubbhamJain <shubham.jain368@gmail.com>
2024-05-15 17:27:48 +0530

    add: dynamic colors

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+
+const colorSet = [
+  {
+    bg: "bg-slate-400",
+    border: "border-gray-400",
+  },
+  {
+    bg: "bg-sky-400",
+    border: "border-sky-500",
+  },
+  {
+    bg: "bg-rose-400",
+    border: "border-rose-500",
+  },
+  {
+    bg: "bg-violet-400",
+    border: "border-violet-500",
+  },
+  {
+    bg: "bg-amber-400",
+    border: "border-amber-500",
+  },
+];
+
 export default function ProfilePic({ text }: { text: string }) {
+  const [colorIndex, setColorIndex] = useState(-1);
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * 5);
+    setColorIndex(index);
+  }, []);
+
   return (
-    <div className="flex items-center justify-center uppercase w-[30px] h-[30px] rounded-[50%] border-gray-400 border-2 bg-slate-400">
+    <div
+      className={`flex items-center justify-center uppercase w-[30px] h-[30px] rounded-[50%] ${colorSet[colorIndex]?.bg} ${colorSet[colorIndex]?.border}`}
+    >
       {text}
     </div>
   );
